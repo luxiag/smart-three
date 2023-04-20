a1c916b9b298ef2810f01c4723bb733d6a0908d1
luxiag <luxiag@qq.com>
2023-04-20 21:49:50 +0800

    style: 主题固定

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,21 +1,53 @@
 <template>
   <div class="app">
-    <div class="header"><h2>数字孪生智慧广州管理平台</h2></div>
+    <div class="header">
+      <div class="header-bg">
+        <div class="title">数字孪生智慧广州管理平台</div>
+      </div>
+    </div>
     <div class="btn-func">
       <div class="btn">
-        <button class="diamond-btn">城市全景</button>
+        <button class="class-btn">城市全景</button>
       </div>
-      <div class="btn"><button class="diamond-btn">城市交通</button></div>
-      <div class="btn"><button class="diamond-btn">城市规划</button></div>
-      <div class="btn"><button class="diamond-btn">城市管理</button></div>
-      <div class="btn"><button class="diamond-btn">城市区域</button></div>
+      <div class="btn"><button class="class-btn active">城市交通</button></div>
+      <div class="btn">
+        <button
+          :class="route.path == '/cityScene' ? 'class-btn active' : 'class-btn'"
+        >
+          城市规划
+        </button>
+      </div>
+      <div class="btn">
+        <button
+          :class="route.path == '/cityScene' ? 'class-btn active' : 'class-btn'"
+        >
+          城市管理
+        </button>
+      </div>
+      <div class="btn">
+        <button
+          :class="route.path == '/cityScene' ? 'class-btn active' : 'class-btn'"
+        >
+          城市区域
+        </button>
+      </div>
     </div>
     <div class="container">
       <router-view></router-view>
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+const router = useRouter();
+const route = useRoute();
+
+const goPage = (path) => {
+  router.push({ path });
+};
+onMounted(() => {
+  console.log(route.path, "route");
+});
+</script>
 <style lang="less">
 .app {
   height: 100%;
@@ -30,8 +62,45 @@
   z-index: 11;
   .btn {
     margin: 0 15px;
+    .class-btn {
+      color: #fff;
+      background-color: transparent;
+      border: none;
+    }
+
+    .class-btn.active,
+    .class-btn:hover {
+      position: relative;
+      display: inline-block;
+      /* padding: 5px 10px; */
+      color: #fff;
+
+      color: #86dbe3;
+      cursor: pointer;
+      /* transform: skew(-20deg); */
+      /* border-radius: 0; */
+      /* box-shadow: 0 5px 0 rgba(0, 0, 0, 0.3); */
+      transition: box-shadow 0.2s ease-in-out;
+      text-shadow: 0 0 20px #29b6f6;
+      &::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: -1px;
+        width: 100%;
+        /* box-shadow: 0px 3px 0px 0px rgba(0, 0, 0, 0.2); */
+        height: 100%;
+        border: 1px solid #7dd1d9;
+        background: rgba(29, 53, 56, 0.6);
+        transform: skew(-20deg);
+        box-shadow: 0 0 5px #7dd1d9;
+        /* border-image: linear-gradient(45deg, #fff, #7dd1d9) 1; */
+        z-index: -1;
+      }
+    }
   }
 }
+
 .header {
   //   padding: 20px 0;
   text-align: center;
@@ -51,6 +120,23 @@
     text-shadow: 0 0 20px #29b6f6;
     // animation: flicker 2s infinite;
   }
+  .header-bg {
+    padding: 0 180px;
+    background: radial-gradient(circle, #5b96b4, transparent);
+    margin-bottom: 10px;
+  }
+  .header-bg::before {
+
+  }
+  .title {
+    padding: 10px 20px;
+    margin: 0;
+    font-weight: 700;
+    font-size: 1.1em;
+    // border-bottom: 5px solid #afd7f0;
+    // margin-bottom: 10px;
+    // background: radial-gradient(circle, #5b96b4, #344a52);
+  }
 }
 
 .container {
@@ -64,6 +150,38 @@
     height: 100%;
   }
 }
+
+.parallelogram {
+  width: 200px;
+  height: 100px;
+  background: #0074d9;
+  transform: skew(-20deg);
+  position: relative;
+}
+
+.parallelogram::before,
+.parallelogram::after {
+  content: "";
+  position: absolute;
+  width: 0;
+  height: 0;
+  border-style: solid;
+}
+
+.parallelogram::before {
+  top: 0;
+  left: -20px;
+  border-width: 0 20px 100px 0;
+  border-color: transparent #0074d9 transparent transparent;
+}
+
+.parallelogram::after {
+  bottom: 0;
+  right: -20px;
+  border-width: 100px 0 0 20px;
+  border-color: transparent transparent transparent #0074d9;
+}
+
 @keyframes flicker {
   0% {
     text-shadow: 0 0 20px #00ffea;
--- src/pages/cityScene/index.vue
+++ src/pages/cityScene/index.vue
@@ -1,11 +1,23 @@
 <template>
-  <div class="left"></div>
+  <div class="left">
+    <div class="left-container"></div>
+    <div class="use-list">
+      <div
+        :class="activeCompName == 'environment' ? 'use active' : 'use'"
+        @click="switchComponent('environment')"
+      >
+        行政规划
+      </div>
+      <div
+        :class="activeCompName == 'regionalPopulation' ? 'use active' : 'use'"
+        @click="switchComponent('regionalPopulation')"
+      >
+        生态环境
+      </div>
+    </div>
+  </div>
   <div class="right"></div>
   <div class="center">
-    <div class="use-list">
-      <div class="use">行政规划</div>
-      <div class="use">生态环境</div>
-    </div>
     <component :is="activeComponent[activeCompName]"></component>
   </div>
 </template>
@@ -17,8 +29,53 @@ const activeComponent = {
   environment,
   regionalPopulation,
 };
+
+const switchComponent = (componentName) => {
+  activeCompName.value = componentName;
+};
 </script>
 <style scoped lang="less">
+.left {
+  padding-top: 90px;
+  position: absolute;
+  z-index: 11;
+  .use-list {
+    padding: 20px;
+    .use {
+      position: relative;
+      margin: 10px 0;
+      padding: 1px 5px;
+      cursor: pointer;
+      &:hover {
+        background: #5d838f;
+      }
+      &.active {
+        background: #5d838f;
+      }
+    }
+    .use::after {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: -1px;
+      width: 100%;
+      /* box-shadow: 0px 3px 0px 0px rgba(0, 0, 0, 0.2); */
+      height: 100%;
+      border: 1px solid #fff;
+      background: rgba(29, 53, 56, 0.6);
+      background: linear-gradient(left right, #404435, transparent);
+      transform: skew(-20deg);
+      box-shadow: 0 1px 5px #fff;
+      border-right: none;
+      text-shadow: 0 0 20px #29b6f6;
+      // border-image: linear-gradient(45deg, #fff, #7dd1d9) 1;
+      z-index: -1;
+    }
+    .active.use::after {
+      background: #5d838f;
+    }
+  }
+}
 .center {
   position: relative;
   .use-list {
