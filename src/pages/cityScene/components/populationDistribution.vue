c324c0fd3a1a07e2dfe25c4869400884add0b40d
luxiag <luxiag@qq.com>
2023-04-25 10:29:41 +0800

    feat:add 3d map

--- src/pages/cityScene/components/populationDistribution.vue
+++ src/pages/cityScene/components/populationDistribution.vue
@@ -25,6 +25,8 @@ const geoMapRef = ref();
 
 const initScene = () => {
   scene = new THREE.Scene();
+  scene.background = null;
+
   camera = new THREE.PerspectiveCamera(
     45,
     window.innerWidth / window.innerHeight,
@@ -35,30 +37,37 @@ const initScene = () => {
   // scene.background = new THREE.Color(0xff00ff);
   scene.add(camera);
 
-  const light = new THREE.AmbientLight(0xffffff, 0.5); // soft white light
-  scene.add(light);
-
-  renderer = new THREE.WebGLRenderer();
-  renderer.shadowMap.enabled = true;
-  renderer.shadowMap.type = THREE.BasicShadowMap;
-  renderer.shadowMap.type = THREE.VSMShadowMap;
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.shadowMap.enabled = true; // 开启阴影
+  renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+  renderer.toneMapping = THREE.ACESFilmicToneMapping;
+  renderer.toneMappingExposure = 1.25;
+  // this.renderer.outputEncoding = THREE.sRGBEncoding;
+  renderer.outputEncoding = THREE.sHSVEncoding;
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(window.innerWidth, window.innerHeight);
-
+  renderer.setClearColor(0xffffff, 0);
   geoMapRef.value.appendChild(renderer.domElement);
   renderer.render(scene, camera);
+  window.addEventListener('resize',onWindowResize)
 };
+const onWindowResize = () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render()
+}
 const initMap = (jsonData) => {
   const features = jsonData.features;
   const projection = d3
     .geoMercator()
     .center([113.264434, 23.129162])
     .translate([0, 0, 0]);
-  const color = "#99ff99";
-
+  // const color = "#99ff99";
+  const COLOR_ARR = ["#0465BD", "#357bcb", "#3a7abd"];
+  const HIGHT_COLOR = "#4fa5ff";
   // region.position.set(center[0],center[1],1)
 
-  features.forEach((feature) => {
+  features.forEach((feature, index) => {
+    const color = COLOR_ARR[index % COLOR_ARR.length];
     const region = new THREE.Object3D();
     region.properties = feature.properties;
     const coordinates = feature.geometry.coordinates;
@@ -67,6 +76,9 @@ const initMap = (jsonData) => {
         coordinate.forEach((rows) => {
           const mesh = drawExtrudeMesh(rows, color, projection);
           mesh.properties = feature.properties;
+          if (index % 2 === 0) {
+            mesh.scale.set(1, 1, 1.2);
+          }
           region.add(mesh);
         });
       });
@@ -74,8 +86,73 @@ const initMap = (jsonData) => {
     map.add(region);
   });
   map.scale.set(10, 10, 10);
-  console.log(map);
   scene.add(map);
+  createPlayGround();
+  createLight();
+};
+
+const createPlayGround = () => {
+  const groundMaterial = new THREE.MeshStandardMaterial({
+    color: 0x031837,
+    // specular: 0x111111,
+    metalness: 0,
+    roughness: 1,
+    // opacity: 0.2,
+    opacity: 0.5,
+    transparent: true,
+  });
+  const ground = new THREE.Mesh(
+    new THREE.PlaneGeometry(2000, 2000, 1, 1),
+    groundMaterial
+  );
+  // ground.rotation.x = - Math.PI / 2;
+  ground.position.z = 0;
+  // ground.castShadow = true;
+  ground.receiveShadow = true;
+
+  scene.add(ground);
+};
+
+const createLight = () => {
+  let ambientLight = new THREE.AmbientLight(0xffffff, 1); // 环境光
+
+  const light = new THREE.DirectionalLight(0xffffff, 0.5); // 平行光
+  light.position.set(20, -50, 20);
+
+  light.castShadow = true;
+  light.shadow.mapSize.width = 1024;
+  light.shadow.mapSize.height = 1024;
+
+  // 半球光
+  let hemiLight = new THREE.HemisphereLight("#80edff", "#75baff", 0.3);
+  // 这个也是默认位置
+  hemiLight.position.set(20, -50, 0);
+  scene.add(hemiLight);
+
+  const pointLight = new THREE.PointLight(0xffffff, 0.5);
+  pointLight.position.set(20, -50, 50);
+
+  pointLight.castShadow = true;
+  pointLight.shadow.mapSize.width = 1024;
+  pointLight.shadow.mapSize.height = 1024;
+
+  const pointLight2 = new THREE.PointLight(0xffffff, 0.5);
+  pointLight2.position.set(50, -50, 20);
+  pointLight2.castShadow = true;
+  pointLight2.shadow.mapSize.width = 1024;
+  pointLight2.shadow.mapSize.height = 1024;
+
+  const pointLight3 = new THREE.PointLight(0xffffff, 0.5);
+  pointLight3.position.set(-50, -50, 20);
+  pointLight3.castShadow = true;
+  pointLight3.shadow.mapSize.width = 1024;
+  pointLight3.shadow.mapSize.height = 1024;
+
+  scene.add(ambientLight);
+  scene.add(light);
+  scene.add(pointLight);
+  scene.add(pointLight2);
+  scene.add(pointLight3);
 };
 
 const createControls = () => {
@@ -94,19 +171,48 @@ const drawExtrudeMesh = (polygon, color, projection) => {
     }
     shape.lineTo(x, -y);
   });
-
-  const geometry = new THREE.ExtrudeGeometry(shape, {
+  // const depth = Math.random() > 0.5 ? 0.1 : 0.2;
+  // console.log(depth, "depth");
+  // const geometry = new THREE.ExtrudeGeometry(shape, {
+  //   depth: 0.1,
+  //   bevelEnabled: false,
+  // });
+  // const randomColor = (0.5 + Math.random() * 0.5) * 0xffffff;
+  // const material = new THREE.MeshBasicMaterial({
+  //   color,
+  //   transparent: true,
+  //   // opacity: 0.5,
+  //   // wireframe:true
+  // });
+  // return new THREE.Mesh(geometry, material);
+  const extrudeSettings = {
     depth: 0.1,
     bevelEnabled: false,
+    bevelSegments: 1,
+    bevelThickness: 0.2,
+  };
+
+  const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+
+  const material = new THREE.MeshStandardMaterial({
+    clearcoat: 3.0,
+    metalness: 1,
+    color: color,
   });
-  const randomColor = (0.5 + Math.random() * 0.5) * 0xffffff;
-  const material = new THREE.MeshBasicMaterial({
-    color: "#205977",
-    transparent: true,
-    opacity: 0.5,
-    // wireframe:true
+
+  const material1 = new THREE.MeshStandardMaterial({
+    clearcoat: 3.0,
+    metalness: 1,
+    roughness: 1,
+    color: color,
   });
-  return new THREE.Mesh(geometry, material);
+
+  const mesh = new THREE.Mesh(geometry, [material, material1]);
+
+  mesh.castShadow = true;
+  mesh.receiveShadow = true;
+  mesh._color = color;
+  return mesh;
 };
 
 const animate = () => {
